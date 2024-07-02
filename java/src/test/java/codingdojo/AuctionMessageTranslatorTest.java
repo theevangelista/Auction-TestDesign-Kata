3fa41c52a5f2f5a87822f34745a8e59485e2e8dc
Evangelista <evangelistajoaop@gmail.com>
2024-07-02 15:57:19 -0300

    add tests to validate the code

--- java/src/test/java/codingdojo/AuctionMessageTranslatorTest.java
+++ java/src/test/java/codingdojo/AuctionMessageTranslatorTest.java
@@ -1,18 +1,35 @@
 package codingdojo;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class AuctionMessageTranslatorTest {
+class AuctionMessageTranslatorTest {
+
+    private AuctionEventListener mockListener;
+    private AuctionMessageTranslator translator;
+
+    @BeforeEach
+    void setUp() {
+        mockListener = mock(AuctionEventListener.class);
+        translator = new AuctionMessageTranslator(mockListener);
+    }
+
+
     @Test
-    public void notifiesAuctionClosedWhenCloseMessageReceived() {
+    void notifiesAuctionClosedWhenCloseMessageReceived() {
         String message = "SOLVersion: 1.1; Event: CLOSE;";
-        // TODO: write a test for this message translation
+        translator.processMessage(message);
+        verify(mockListener).auctionClosed();
     }
 
     @Test
     public void notifiesBidDetailsWhenPriceMessageReceived() {
-        String message = "SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;";
-        // TODO: write a test for this message translation
+        String message =
+                "SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;";
+        translator.processMessage(message);
+        verify(mockListener).currentPrice(192);
     }
 }
 
